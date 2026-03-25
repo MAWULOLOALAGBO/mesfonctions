770bf6519b130f38274c0059cecfae5e8ef1a10b
Mawulolo ALAGBO <parfaitalagbo18@gmail.com>
2026-03-25 12:04:23 +0100

    Update histogram_boxplot_var_quanti.R

--- histogram_boxplot_var_quanti.R
+++ histogram_boxplot_var_quanti.R
@@ -1,13 +1,33 @@
 his_box <- function(df_num){
+  
   vect_num <- names(df_num)
-  for(i in names(df_num)){
-    graph <- ggplot(df_num,
-                    aes(x=df_num[[i]]))+
-      geom_histogram(fill = "steelblue",aes(y=..density..))+
-      geom_density(color = "red",linewidth = 0.8)+
-      labs(title = paste("repartition de ",i),
-           x=i)
-    print(graph)
+  
+  # listes vides pour stocker les plots
+  histo_list <- list()
+  boxp_list <- list()
+  
+  # Boucle histogrammes
+  for(i in vect_num){
+    p <- ggplot(df_num, aes(x = .data[[i]])) +
+      geom_histogram(fill = "steelblue", aes(y = ..density..)) +
+      geom_density(color = "red", linewidth = 0.8) +
+      labs(title = paste("Répartition de", i),
+           x = i)
+    
+    histo_list[[i]] <- p
   }
-  return(graph)
+  
+  # Boucle boxplots
+  for(j in vect_num){
+    p <- ggplot(df_num, aes(y = .data[[j]])) +
+      geom_boxplot(fill = "darkblue") +
+      labs(title = paste("Dispersion de", j),
+           y = j)
+    
+    boxp_list[[j]] <- p
+  }
+  
+  # Retourner une liste de listes
+  return(list(histogrammes = histo_list,
+              boxplots = boxp_list))
 }
